de8843affe5d182b67447923a404c5d4dbe40d25
Zhongzheng <zhongzhengwang@vlan-2688-10-17-105-104.staff.wireless.sydney.edu.au>
2018-03-13 16:02:36 +1100

    superLattice2D update

---
+++ test_numpy.py
@@ -0,0 +1,22 @@
+import numpy
+
+
+
+
+
+
+
+# def getMaterialCoords():
+# 	materialCoords = numpy.zeros([0,2])
+# 	for x in range(5):
+# 		for y in range(10):
+# 			print(x)
+# 			print(y)
+# 			print(materialCoords.shape)
+# 			print(numpy.array([x,y]))
+# 			materialCoord = numpy.append(materialCoords,numpy.array([x,y]),0)
+# 	return materialCoords
+
+
+# materialCoords = getMaterialCoords()
+# print(materialCoords)
